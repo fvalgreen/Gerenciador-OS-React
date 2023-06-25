23df7a10ef1615eaf74b1030b129d1f9d84a83fa
fvalgreen <valverde130495@gmail.com>
2023-06-25 13:10:50 -0300

    feat(components): PublicInput

---
+++ src/components/PublicInput.tsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+type PublicInputType = {
+  icon: string;
+  alt?: string;
+  name: string;
+  type: string;
+  modelValue: string;
+  setValue(s: string): void;
+}
+
+const PublicInput: React.FC<PublicInputType> = ({
+  icon,
+  name,
+  type,
+  alt,
+  modelValue,
+  setValue
+}) => {
+  const [focus, setFocus]= useState(false)
+  return (
+    <div className={"input-public " + (focus? 'focus' : '')}>
+      <img src={icon} alt={alt} />
+      <input 
+        type={type} 
+        placeholder={name} 
+        onFocus={() => setFocus(true)}
+        onBlur={() => setFocus(false)}
+        onChange={(e) => setValue(e.target.value)}
+      />
+    </div>
+  );
+};
+
+export default PublicInput;
