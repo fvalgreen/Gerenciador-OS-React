632e497e8fbaeefd31edc85cb207977e21abc9d6
fvalgreen <valverde130495@gmail.com>
2023-06-27 15:35:32 -0300

    feat(assets/images): icons

--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
-import mail from "../assets/images/envelope.svg";
-import key from "../assets/images/chave.svg";
+import mail from "../assets/images/user.svg";
+import key from "../assets/images/lock.svg";
 import logo from "../assets/images/logo.svg";
 import PublicInput from "../components/PublicInput";
 
@@ -23,9 +23,16 @@ const Login = () => {
         </div>
         <form>
           <PublicInput icon={mail} name="Email" type="text" alt="Email" modelValue={login} setValue={setLogin} />
-          <PublicInput icon={key} name="Senha" type="password" alt="Senha" modelValue={password} setValue={setPassword} />
+          <PublicInput icon={key} name="Senha" type="password" alt="Senha" modelValue={password} setValue={setPassword} />          
+          {error && 
+            <p className="error">{error}</p>
+          }
           <button type="button" onClick={doLogin}>Login</button>
         </form>
+        <div className="register-info">
+          <p>Não possui conta?</p>
+          <span>Cadastre-se agora!</span>
+        </div>
       </div>
     </>
   );
